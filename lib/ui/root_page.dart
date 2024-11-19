08cd0e443d35a0d2fd90aed962bf3be6033db8ac
CLS11 <crystall.101600@gmail.com>
2024-11-19 15:46:23 +0530

    Root page using page transitions.

---
+++ lib/ui/root_page.dart
@@ -0,0 +1,103 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/ui/scan_page.dart';
+import 'package:plant_app/ui/screens/cart_page.dart';
+import 'package:plant_app/ui/screens/favorite_page.dart';
+import 'package:plant_app/ui/screens/home_page.dart';
+import 'package:plant_app/ui/screens/profile_page.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _bottomNavIndex = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    FavoritePage(),
+    CartPage(),
+    ProfilePage(),
+  ];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+  List<String> titleList = [
+    'Home',
+    'Favorite',
+    'Cart',
+    'Profile',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              titleList[_bottomNavIndex],
+              style: TextStyle(
+                color: Constants.blackColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 24,
+              ),
+            ),
+            Icon(
+              Icons.notifications,
+              color: Constants.blackColor,
+              size: 30,
+            ),
+          ],
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+      ),
+      body: IndexedStack(
+        index: _bottomNavIndex,
+        children: pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            PageTransition(
+              child: const ScanPage(),
+              type: PageTransitionType.bottomToTop,
+            ),
+          );
+        },
+        backgroundColor: Constants.primaryColor,
+        child: Image.asset(
+          'assets/code-scan-two.png',
+          height: 30,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        inactiveColor: Colors.black.withOpacity(0.5),
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        onTap: (index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
