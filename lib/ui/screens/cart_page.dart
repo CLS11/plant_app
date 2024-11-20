67e531381f15dd1153eab278460e85a0ee8a8313
CLS11 <crystall.101600@gmail.com>
2024-11-20 23:55:20 +0530

    Add to cart page and shows the items present in the cart.

--- lib/ui/screens/cart_page.dart
+++ lib/ui/screens/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,9 +11,29 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Cart Page'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+              child: Image.asset('assets/add-cart.png'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Your Cart is Empty',
+              style: TextStyle(
+                color: Constants.primaryColor,
+                fontWeight: FontWeight.w300,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
