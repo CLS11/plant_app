0b3ccdffc722d3804a11823447c671a886a103fe
CLS11 <crystall.101600@gmail.com>
2024-11-21 19:03:44 +0530

    Added a plant widget.

--- lib/ui/screens/cart_page.dart
+++ lib/ui/screens/cart_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/models/plants.dart';
 
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+  const CartPage({required this.addedToCartPlants, super.key});
+  final List<Plants> addedToCartPlants;
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -11,30 +13,84 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 100,
-              child: Image.asset('assets/add-cart.png'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Your Cart is Empty',
-              style: TextStyle(
-                color: Constants.primaryColor,
-                fontWeight: FontWeight.w300,
-                fontSize: 18,
+      body: widget.addedToCartPlants.isEmpty
+          ? Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset('assets/add-cart.png'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Your Cart is Empty',
+                    style: TextStyle(
+                      color: Constants.primaryColor,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 30,
+              ),
+              height: size.height,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.addedToCartPlants.length,
+                      scrollDirection: Axis.vertical,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return PlantWidget(
+                          index: index,
+                          plantList: widget.addedToCartPlants,
+                        );
+                      },
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const Divider(
+                        thickness: 1,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Totals',
+                            style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          Text(
+                            r'$65',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Constants.primaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
